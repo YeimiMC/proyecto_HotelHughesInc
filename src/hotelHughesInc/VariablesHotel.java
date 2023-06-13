5111182a1f5df758282ad3ea95860808b5616e0c
Yeimi Magadan <mcyeimi99@gmail.com>
2023-06-12 18:39:37 -0600

    Proyecto del Hotel Hughes: primeras variables

---
+++ src/hotelHughesInc/VariablesHotel.java
@@ -0,0 +1,25 @@
+package hotelHughesInc;
+
+public class VariablesHotel {
+
+	public static void main(String[] args) {
+		// byte, short, int, long, double, float
+		
+		byte numPisos= 18;
+		
+		short habitaciones= 180;
+		
+		int huespedesAnuales= 56979;
+
+		long huepedesMundiales = 345678267l;
+		
+		float costoHabitacion = 4999.99f;
+		
+		double costoSuit = 149999.99;
+		
+		//boolean habitacionLibre = true;
+		
+
+	}
+
+}
